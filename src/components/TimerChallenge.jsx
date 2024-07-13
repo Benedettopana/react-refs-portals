d8d153f6770576f30ea73276ed9173de013cbea9
Benedettopana <benedetto.pana@gmail.com>
2024-07-13 10:54:42 +0200

    [add]start/stop manually || automatically

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -7,16 +7,22 @@ export default function TimerChallenge({ title, targetTime }) {
 
   const [timeRemaning, setTimeRemening] = useState(targetTime * 1000);
 
+  const timerIsActive = timeRemaning > 0 && timeRemaning < targetTime * 1000;
+
+  if (timeRemaning <= 0) {
+    clearInterval(timer.current);
+    setTimeRemening(targetTime * 1000);
+    dialog.current.open();
+  }
+
   function handleStart() {
     timer.current = setInterval(() => {
-      setTimerExpired(true);
-      dialog.current.open();
+      setTimeRemening((prevTimeRemaning) => prevTimeRemaning - 10);
     }, 10);
-
-    setTimerStarted(true);
   }
 
   function handleStop() {
+    dialog.current.open();
     clearTimeout(timer.current);
   }
 
@@ -25,17 +31,16 @@ export default function TimerChallenge({ title, targetTime }) {
       <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You Lost!</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "actione" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
