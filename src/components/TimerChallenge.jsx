bee7e55b2c27ea6aae17e67b25b250ed974dd266
Benedettopana <benedetto.pana@gmail.com>
2024-07-13 11:30:12 +0200

    [add]player score

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -5,19 +5,22 @@ export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const [timeRemaning, setTimeRemening] = useState(targetTime * 1000);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-  const timerIsActive = timeRemaning > 0 && timeRemaning < targetTime * 1000;
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-  if (timeRemaning <= 0) {
+  if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemening(targetTime * 1000);
     dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
     timer.current = setInterval(() => {
-      setTimeRemening((prevTimeRemaning) => prevTimeRemaning - 10);
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
     }, 10);
   }
 
@@ -28,7 +31,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
